8344978917749e210fb6eefd1d3e36b00f13d37f
OAseongjun <psj7557@naver.com>
2024-03-26 00:29:23 +0000

    update ver.1

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -31,7 +31,7 @@ function getUserIP(req) {
 
 var userlogs = new UserLogs();
 
-userlogs.ID = '테스터';
+userlogs.ID = '박성준';
 userlogs.save(err => {
 	if (!err) console.log(`success`)
 })
@@ -55,6 +55,39 @@ exports.userInfo = function (req, res) {
 	res.status(200).send('wkit get Method')
 
 }
+
+
+
+/*const mongoose = require('mongoose');
+const UserLogs = mongoose.model('UserLogs');
+const UserInfo = mongoose.model('UserInfo');
+
+function getUserIP(req) {
+    let ipAddress;
+    // IP 주소를 가져오는 코드 작성
+    return ipAddress;
+}
+
+// UserLogs 모델의 인스턴스를 생성하고 저장하는 부분 수정
+const userlogs = new UserLogs({ ID: '박성준' }); // ID를 생성자에 전달하여 인스턴스 생성
+userlogs.save(err => {
+    if (err) {
+        console.error('Error saving user logs:', err);
+    } else {
+        console.log('User logs saved successfully');
+    }
+});
+
+// userLogs 및 userInfo 미들웨어 함수 수정
+exports.userLogs = function (req, res) {
+    // req.body = {test : test}
+    const result = req.body.test;
+    // 적절한 응답을 보내는 코드 작성
+};
+
+exports.userInfo = function (req, res) {
+    // 적절한 응답을 보내는 코드 작성
+};*/
 
 
 
--- config/mongoose_config.js
+++ config/mongoose_config.js
@@ -1,25 +1,26 @@
 var config = {
-	    db: "mongodb+srv://test:1234@cluster0.odczpuc.mongodb.net/Databases"
+            db: "mongodb+srv://test:1234@cluster0.odczpuc.mongodb.net/Databases"
 };
 
 var mongoose = require('mongoose');
 
 module.exports = function(){
-	    var db = mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true});
-	    console.log("Connected MongoDB ver: " + mongoose.version);
-	    mongoose.set('useCreateIndex', true);
+            var db = mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true});
+            console.log("Connected MongoDB ver: " + mongoose.version);
+            mongoose.set('useCreateIndex', true);
 
-	    require('../app/models/UserInfo.js');
-	    require('../app/models/UserLogs.js');
-
-
-	 var UserInfo = mongoose.model('UserInfo');
-	            UserInfo.find({}, (err, users) => {
-			                                if (err) {
-								                                                console.error('Error querying UserInfo:', err);						                                          } else {														                                                                    console.log('User Info:', users);
-								                                                              }
-			                            });
+            require('../app/models/UserInfo.js');
+            require('../app/models/UserLogs.js');
 
 
-	    return db;
+         var UserInfo = mongoose.model('UserInfo');
+                    UserInfo.find({}, (err, users) => {    
+                            if (err) { console.error('Error querying UserInfo:', err);                                                   }
+ else{                                 
+             console.log('User Info:', users);                
+                }
+                                                    });
+
+
+            return db;
 }
